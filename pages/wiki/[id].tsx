cd7fdddab39ded329f121b42516459224f24e8d0
AdamSeungheonShin <ssh9809@naver.com>
2024-06-22 17:01:07 +0900

    feat/test api and declare type of profile data response body

---
+++ pages/wiki/[id].tsx
@@ -0,0 +1,30 @@
+import axios from "@/apis/axios";
+import { ProfileResponse } from "@/types/ProfileResponse";
+import { useEffect, useState } from "react";
+
+const TEST_CODE: string = "77348674-31f5-4d09-8c1c-c92a1af25f63";
+// 테스트 완료 후, code 변수 추가 할 때 삭제 예정
+
+export default function Wiki() {
+  const [data, setData] = useState<ProfileResponse>();
+
+  useEffect(() => {
+    const getWikiDataByCode = async () => {
+      try {
+        const { data } = await axios.get(`profiles/${TEST_CODE}`);
+        // 프로필 데이터 호출 url path의 code 부분 변수로 수정 예정, 테스트용
+        if (data) {
+          setData(data);
+        } else {
+          return;
+        }
+      } catch (e) {
+        console.error("failed to fetch", e);
+      }
+    };
+
+    getWikiDataByCode();
+  }, []);
+
+  return <div>{data?.content}</div>;
+}
---
+++ src/types/ProfileResponse.d.ts
@@ -0,0 +1,19 @@
+export type ProfileResponse = {
+  updatedAt: string;
+  securityQuestion: string;
+  teamId: string;
+  content: string;
+  nationality: string;
+  family: string;
+  bloodType: string;
+  nickname: string;
+  birthday: string;
+  sns: string;
+  job: string;
+  mbti: string;
+  city: string;
+  image: string;
+  code: string;
+  name: string;
+  id: number;
+};
