bdea70b6de25c23ccb0483f63f3e14fa43688530
AdamSeungheonShin <ssh9809@naver.com>
2024-06-22 12:22:30 +0900

    feat/tailwind-set-custom-color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,27 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        gray: {
+          100: "#F7F7FA",
+          200: "#E4E5F0",
+          300: "#C6CADA",
+          400: "#8F95B2",
+          500: "#474D66",
+          600: "#3B415B",
+        },
+        green: {
+          100: "#EEF9F6",
+          200: "#4CBFA4",
+          300: "#32A68A",
+        },
+        red: {
+          100: "#FBEDED",
+          200: "#D14343",
+        },
+        purple: "#8E66FF",
+        yellow: "#FDD181",
+      },
     },
   },
   plugins: [],
