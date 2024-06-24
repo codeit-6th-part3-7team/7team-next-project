baa651246904e4f64855e024b57d3016e6cbf4c6
sohee <topa11uu@gmail.com>
2024-06-24 12:58:17 +0900

    feat: wikilist, UserCard 파일 생성

---
+++ pages/wiki/wikiList.tsx
@@ -0,0 +1,12 @@
+import UserCard from "@/constants/userCard";
+
+export default function wikiList() {
+  return (
+    <div>
+      <div>헤더</div>
+      <div>검색</div>
+      <UserCard />
+      <div>페이지네이션</div>
+    </div>
+  );
+}
---
+++ src/constants/UserCard.tsx
@@ -0,0 +1,3 @@
+export default function UserCard() {
+  return <div>유저카드</div>;
+}
