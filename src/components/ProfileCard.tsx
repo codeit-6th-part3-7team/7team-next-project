1ea4a28ccd695c7b3cf6e7c8407be03d88954f7d
AdamSeungheonShin <ssh9809@naver.com>
2024-06-24 20:52:25 +0900

    feat: 프로필 조건부 렌더링 이미지 변경

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import temp from "../../public/img_temp_profile.png";
+import ic_profile_skeleton from "../../public/ic_profile_skeleton.webp";
 import { ProfileCardData } from "@/types/ProfileResponse";
 
 type ProfileCardProps = {
@@ -21,7 +21,7 @@ const labels: Record<keyof ProfileCardData, string> = {
 export default function ProfileCard({ profileData, profileImage }: ProfileCardProps) {
   return (
     <div className="w-80 h-[670px] rounded-xl shadow-lg shadow-gray-300 flex flex-col items-center gap-14 border-none absolute top-32 right-[3%]">
-      <Image className="w-[200px] h-[200px] object-cover rounded-full mt-14" src={profileImage ? profileImage : temp} width={200} height={200} alt="프로필이미지" />
+      <Image className="w-[200px] h-[200px] object-cover rounded-full mt-14" src={profileImage ? profileImage : ic_profile_skeleton} width={200} height={200} alt="프로필이미지" />
       <div className="w-64 h-6 grid grid-cols-[75px_auto] gap-4">
         {Object.entries(profileData).map(([key, value]) => {
           return (
