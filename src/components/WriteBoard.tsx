a660906e154422e03f730be42d4c7329a0b131c9
Lee Youl <2yulrang@gmail.com>
2024-06-25 20:02:29 +0900

    fat: 에디터 수정 중, 색상선택 추가

--- src/components/WriteBoard.tsx
+++ src/components/WriteBoard.tsx
@@ -1,13 +1,28 @@
+import Image from "next/image";
 import { Color } from "@tiptap/extension-color";
 import ListItem from "@tiptap/extension-list-item";
 import TextStyle from "@tiptap/extension-text-style";
 import TextAlign from "@tiptap/extension-text-align";
+import UnderLine from "@tiptap/extension-underline";
 import { Editor, EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { Box, Button, Input } from "@mantine/core";
-import { ChangeEvent, useState } from "react";
+import { ActionIcon, Box, Button, ColorPicker, Flex, Input, Modal } from "@mantine/core";
+import { useState } from "react";
+import iconBold from "@/public/assets/ic_bold.svg";
+import iconItalic from "@/public/assets/ic_italic.svg";
+import iconUnderline from "@/public/assets/ic_underline.svg";
+import iconCenter from "@/public/assets/ic_Alignment_center.svg";
+import iconRight from "@/public/assets/ic_Alignment_right.svg";
+import iconBulletList from "@/public/assets/ic_Bullet.svg";
+import iconOrderedList from "@/public/assets/ic_numbering.svg";
+import iconColoring from "@/public/assets/ic_coloring.svg";
+import iconImage from "@/public/assets/ic_image.svg";
+import iconLeft from "@/public/assets/ic_Alignment_left.svg";
+import { useDisclosure } from "@mantine/hooks";
 
 const extensions = [
+  UnderLine,
+  TextStyle,
   Color.configure({ types: [TextStyle.name, ListItem.name] }),
   StarterKit.configure({
     bulletList: {
@@ -25,103 +40,128 @@ const extensions = [
 ];
 
 function MenuBar({ editor }: { editor: Editor }) {
+  const [colorPickerOpened, { open: openColorPicker, close: closeColorPicker }] = useDisclosure(false);
+  const [colorValue, handleColorChange] = useState("rgba(47, 119, 150, 0.7)");
+  const [colorClass, setColorClass] = useState("");
+  const handleColoring = () => {
+    setColorClass(editor.isActive("textStyle", { color: colorValue }) ? "is-active" : "");
+    editor.chain().focus().setColor(colorValue).run();
+  };
   return (
     <div className="control-group">
       <div className="button-group">
-        <button
-          type="button"
+        <ActionIcon
+          variant="transparent"
+          size="lg"
+          aria-label="Settings"
           onClick={() => editor.chain().focus().toggleBold().run()}
           disabled={!editor.can().chain().focus().toggleBold().run()}
           className={editor.isActive("bold") ? "is-active" : ""}
         >
-          Bold
-        </button>
-        <button
-          type="button"
+          <Image src={iconBold} alt="굵게" width={24} height={24} />
+        </ActionIcon>
+        <ActionIcon
+          variant="transparent"
+          size="lg"
+          aria-label="Settings"
           onClick={() => editor.chain().focus().toggleItalic().run()}
           disabled={!editor.can().chain().focus().toggleItalic().run()}
           className={editor.isActive("italic") ? "is-active" : ""}
         >
-          Italic
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          disabled={!editor.can().chain().focus().toggleStrike().run()}
-          className={editor.isActive("strike") ? "is-active" : ""}
+          <Image src={iconItalic} alt="기울게" width={24} height={24} />
+        </ActionIcon>
+        <ActionIcon
+          variant="transparent"
+          size="lg"
+          aria-label="Settings"
+          onClick={() => editor.chain().focus().toggleUnderline().run()}
+          disabled={!editor.can().chain().focus().toggleUnderline().run()}
+          className={editor.isActive("underline") ? "is-active" : ""}
         >
-          Strike
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleCode().run()}
-          disabled={!editor.can().chain().focus().toggleCode().run()}
-          className={editor.isActive("code") ? "is-active" : ""}
+          <Image src={iconUnderline} alt="밑줄" width={24} height={24} />
+        </ActionIcon>
+        <ActionIcon
+          variant="transparent"
+          size="lg"
+          aria-label="Settings"
+          onClick={() => editor.chain().focus().setTextAlign("left").run()}
+          className={editor.isActive({ textAlign: "left" }) ? "is-active" : ""}
         >
-          Code
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().unsetAllMarks().run()}>
-          Clear marks
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().clearNodes().run()}>
-          Clear nodes
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().setTextAlign("left").run()} className={editor.isActive({ textAlign: "left" }) ? "is-active" : ""}>
-          Left
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().setTextAlign("center").run()} className={editor.isActive({ textAlign: "center" }) ? "is-active" : ""}>
-          Center
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().setTextAlign("right").run()} className={editor.isActive({ textAlign: "right" }) ? "is-active" : ""}>
-          Right
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().setTextAlign("justify").run()} className={editor.isActive({ textAlign: "justify" }) ? "is-active" : ""}>
-          Justify
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().toggleBulletList().run()} className={editor.isActive("bulletList") ? "is-active" : ""}>
-          Bullet list
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().toggleOrderedList().run()} className={editor.isActive("orderedList") ? "is-active" : ""}>
-          Ordered list
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().toggleBlockquote().run()} className={editor.isActive("blockquote") ? "is-active" : ""}>
-          Blockquote
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-          Horizontal rule
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().setHardBreak().run()}>
-          Hard break
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().undo().run()} disabled={!editor.can().chain().focus().undo().run()}>
-          Undo
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().redo().run()} disabled={!editor.can().chain().focus().redo().run()}>
-          Redo
-        </button>
-        <button type="button" onClick={() => editor.chain().focus().setColor("#958DF1").run()} className={editor.isActive("textStyle", { color: "#958DF1" }) ? "is-active" : ""}>
-          Purple
-        </button>
+          <Image src={iconLeft} alt="왼쪽 정렬" width={24} height={24} />
+        </ActionIcon>
+        <ActionIcon
+          variant="transparent"
+          size="lg"
+          aria-label="Settings"
+          onClick={() => editor.chain().focus().setTextAlign("center").run()}
+          className={editor.isActive({ textAlign: "center" }) ? "is-active" : ""}
+        >
+          <Image src={iconCenter} alt="가운데 정렬" width={24} height={24} />
+        </ActionIcon>
+        <ActionIcon
+          variant="transparent"
+          size="lg"
+          aria-label="Settings"
+          onClick={() => editor.chain().focus().setTextAlign("right").run()}
+          className={editor.isActive({ textAlign: "right" }) ? "is-active" : ""}
+        >
+          <Image src={iconRight} alt="오른쪽 정렬" width={24} height={24} />
+        </ActionIcon>
+        <ActionIcon variant="transparent" size="lg" aria-label="Settings" onClick={() => editor.chain().focus().toggleBulletList().run()} className={editor.isActive("bulletList") ? "is-active" : ""}>
+          <Image src={iconBulletList} alt="글머리 기호" width={24} height={24} />
+        </ActionIcon>
+        <ActionIcon
+          variant="transparent"
+          size="lg"
+          aria-label="Settings"
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive("orderedList") ? "is-active" : ""}
+        >
+          <Image src={iconOrderedList} alt="글번호" width={24} height={24} />
+        </ActionIcon>
+        <ActionIcon variant="transparent" size="lg" aria-label="Settings" onClick={openColorPicker} className={colorClass}>
+          <Image src={iconColoring} alt="글자색" width={24} height={24} />
+        </ActionIcon>
+        <ActionIcon variant="transparent" size="lg" aria-label="Settings">
+          <Image src={iconImage} alt="이미지" width={24} height={24} />
+        </ActionIcon>
       </div>
+      <Modal opened={colorPickerOpened} size="xs" onClose={closeColorPicker} title="Authentication">
+        <Flex direction="column" align="center">
+          <ColorPicker format="rgba" size="lg" value={colorValue} onChange={handleColorChange} />
+          <Button
+            type="submit"
+            variant="outline"
+            color="green"
+            mt={20}
+            px={40}
+            onClick={() => {
+              handleColoring();
+              closeColorPicker();
+            }}
+          >
+            선택
+          </Button>
+        </Flex>
+      </Modal>
     </div>
   );
 }
 
 export default function WriteBoard() {
-  const [length, setLength] = useState(0);
-  const [lengthExceptSpace, setLengthExceptSpace] = useState(0);
+  const [length, setLength] = useState<number | undefined>(0);
+  const [lengthExceptSpace, setLengthExceptSpace] = useState<number | undefined>(0);
   const editor = useEditor({
     extensions,
+    onUpdate: () => {
+      setLength(editor?.getText().length);
+      setLengthExceptSpace(editor?.getText().replace(/ /g, "").length);
+    },
   });
 
   if (!editor) {
     return null;
   }
-
-  const handleChange = (e: ChangeEvent<HTMLDivElement>) => {
-    setLength(e.target.innerText.length);
-    setLengthExceptSpace(e.target.innerText.replace(/ /g, "").length);
-  };
 
   return (
     <div className="flex justify-center align-middle">
@@ -139,7 +179,7 @@ export default function WriteBoard() {
             <span>공백제외 : 총 {lengthExceptSpace}자</span>
           </p>
           <div>
-            <EditorContent editor={editor} onChange={handleChange} placeholder="본문을 입력해주세요." />
+            <EditorContent editor={editor} placeholder="본문을 입력해주세요." className="" />
             <MenuBar editor={editor} />
           </div>
         </Box>
