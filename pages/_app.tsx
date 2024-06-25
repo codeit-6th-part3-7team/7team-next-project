0796ca678eebc9b14aeeb6013a8824bea36c9af8
Lee Youl <2yulrang@gmail.com>
2024-06-25 17:24:31 +0900

    feat: 에디터 초안 작성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Header from "@/src/components/Header";
 import "@/src/styles/globals.css";
 import theme from "@/src/styles/theme";
@@ -5,13 +6,22 @@ import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import { Notifications } from "@mantine/notifications";
 import "@mantine/notifications/styles.css";
+=======
+import "@/src/styles/globals.css";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+>>>>>>> 65715ca (feat: 에디터 초안 작성)
 import type { AppProps } from "next/app";
+import theme from "@/src/styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={theme}>
+<<<<<<< HEAD
       <Notifications position="top-right" />
       <Header />
+=======
+>>>>>>> 65715ca (feat: 에디터 초안 작성)
       <Component {...pageProps} />
     </MantineProvider>
   );
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { MantineThemeOverride, createTheme } from "@mantine/core";
 
 const theme: MantineThemeOverride = {
@@ -71,10 +72,24 @@ const theme: MantineThemeOverride = {
       "#FDD181",
       "#FDD181",
     ],
+=======
+import { createTheme, MantineThemeOverride } from "@mantine/core";
+
+const theme: MantineThemeOverride = {
+  colors: {
+    gray: ["#F7F7FA", "#E4E5F0", "#C6CADA", "#8F95B2", "#474D66", "#3B415B", "#3B415B", "#3B415B", "#3B415B", "#3B415B"],
+    green: ["#EEF9F6", "#4CBFA4", "#32A68A", "#32A68A", "#32A68A", "#32A68A", "#32A68A", "#32A68A", "#32A68A", "#32A68A"],
+    red: ["#FBEDED", "#D14343", "#D14343", "#D14343", "#D14343", "#D14343", "#D14343", "#D14343", "#D14343", "#D14343"],
+    purple: ["#8E66FF", "#8E66FF", "#8E66FF", "#8E66FF", "#8E66FF", "#8E66FF", "#8E66FF", "#8E66FF", "#8E66FF", "#8E66FF"],
+    yellow: ["#FDD181", "#FDD181", "#FDD181", "#FDD181", "#FDD181", "#FDD181", "#FDD181", "#FDD181", "#FDD181", "#FDD181"],
+>>>>>>> 65715ca (feat: 에디터 초안 작성)
   },
   fontFamily: "Pretendard, sans-serif",
 };
 // note  mantine theme 색상 배열타입 기본 길이가 10으로 설정되어 있어서 사용하는 색상 외에는 임의로 채워넣었습니다..
+<<<<<<< HEAD
 // theme.colors.gray[0]
+=======
+>>>>>>> 65715ca (feat: 에디터 초안 작성)
 
 export default createTheme(theme);
