7fbb06f957a3bd72e99b372e545097210125118a
JustDevRae <ksrae165@gmail.com>
2024-06-25 23:14:28 +0900

    refactor: 회원가입 스키마와 타입 별도의 파일로 분류

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -5,25 +5,8 @@ import { notifications } from "@mantine/notifications";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { Controller, useForm } from "react-hook-form";
-import * as z from "zod";
-
-const schema = z
-  .object({
-    name: z
-      .string()
-      .min(2, "이름을 입력해주세요")
-      .max(10, "10자 이하로 작성해주세요")
-      .regex(/^[^\d!@#$%^&*(),.?":{}|<>]*$/, "이름에 숫자나 특수문자가 포함될 수 없습니다."),
-    email: z.string().min(1, "이메일을 입력해주세요").email("이메일 형식으로 작성해 주세요."),
-    password: z.string().min(1, "비밀번호를 입력해주세요").min(8, "8자 이상 입력해주세요."),
-    passwordConfirmation: z.string().min(1, "비밀번호를 입력해주세요").min(8, "8자 이상 입력해주세요."),
-  })
-  .refine((data) => data.password === data.passwordConfirmation, {
-    message: "비밀번호가 일치하지 않습니다.",
-    path: ["passwordConfirmation"],
-  });
-
-type FormData = z.infer<typeof schema>;
+import schema from "@/schema/signup";
+import { SignUpFormData } from "@/types/type";
 
 const showNotification = (title: string, message: string, color: string) => {
   notifications.show({
@@ -56,12 +39,12 @@ export default function SignUp() {
     control,
     handleSubmit,
     formState: { errors, isValid, touchedFields },
-  } = useForm<FormData>({
+  } = useForm<SignUpFormData>({
     resolver: zodResolver(schema),
     mode: "onBlur",
   });
 
-  const onSubmit = async (data: FormData) => {
+  const onSubmit = async (data: SignUpFormData) => {
     try {
       await axios.post("auth/signUp", {
         name: data.name,
@@ -86,7 +69,7 @@ export default function SignUp() {
     }
   };
 
-  const getClassName = (fieldName: keyof FormData) => {
+  const getClassName = (fieldName: keyof SignUpFormData) => {
     if (errors[fieldName]) {
       return "border border-red-500";
     }
