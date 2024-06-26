cb4355c0b17dd7c72fe918376a22b17cfc2c22bf
sohee <topa11uu@gmail.com>
2024-06-26 22:59:56 +0900

    style: 시맨틱 태그 추가 및 코드 변경

--- pages/wiki/wikiList.tsx
+++ pages/wiki/wikiList.tsx
@@ -15,10 +15,7 @@ export default function WikiList() {
   useEffect(() => {
     async function fetchArticles() {
       try {
-        const response = await instance.get(`/profiles?page=${page}&name=${value}`);
-        // TODO: 데이터 전송 확인
-        // eslint-disable-next-line no-console
-        console.log(response.data);
+        const response = await instance.get(`/profiles`, { params: { name: value } });
         if (Array.isArray(response.data.list)) {
           setArticles(response.data.list);
           setTotalPages(Math.ceil(response.data.totalCount / 3));
@@ -41,35 +38,41 @@ export default function WikiList() {
 
   return (
     <div>
-      <div>헤더</div>
-      <div className="flex justify-center align-center mx-[20px]">
-        <div className="relative w-[860px]">
-          <input
-            type="text"
-            name="위키 검색"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            placeholder="이름으로 위키 찾기"
-            className="w-full h-[45px] m-auto bg-gray-100 rounded-20 py-[10px] pl-[55px] outline-none"
-          />
-          <div className="absolute left-[20px] top-1/2 transform -translate-y-1/2">
-            <Image src={searchIcon} alt="검색 아이콘" width={22} height={22} draggable="false" />
+      <header>
+        <div>헤더</div>
+      </header>
+      <main>
+        <section className="flex justify-center align-center mx-[20px]">
+          <div className="relative w-[860px]">
+            <input
+              type="text"
+              name="위키 검색"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              placeholder="이름으로 위키 찾기"
+              className="w-full h-[45px] m-auto bg-gray-100 rounded-20 py-[10px] pl-[55px] outline-none"
+            />
+            <div className="absolute left-[20px] top-1/2 transform -translate-y-1/2">
+              <Image src={searchIcon} alt="검색 아이콘" width={22} height={22} draggable="false" />
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="w-[860px] mb-[60px] m-auto my-[16px] text-[16px] font-[400] text-gray-400">
-        {value ? (
-          <span>
-            &quot;{value}&quot;님을 총 {searchResults.length}명 찾았습니다.
-          </span>
-        ) : (
-          <br />
-        )}
-      </div>
-      <div className="h-[470px]">
-        {value ? searchResults.map((article) => <UserCard key={article.id} article={article} />) : articles.map((article) => <UserCard key={article.id} article={article} />)}
-      </div>
-      <Pagination totalPages={totalPages} currentPage={page} onPageChange={(newPage) => setPage(newPage)} />
+        </section>
+        <section className="w-[860px] mb-[60px] m-auto my-[16px] text-[16px] font-[400] text-gray-400">
+          {value ? (
+            <span>
+              &quot;{value}&quot;님을 총 {searchResults.length}명 찾았습니다.
+            </span>
+          ) : (
+            <br />
+          )}
+        </section>
+        <section className="h-[470px]">
+          {value ? searchResults.map((article) => <UserCard key={article.id} article={article} />) : articles.map((article) => <UserCard key={article.id} article={article} />)}
+        </section>
+        <footer>
+          <Pagination totalPages={totalPages} currentPage={page} onPageChange={(newPage) => setPage(newPage)} />
+        </footer>
+      </main>
     </div>
   );
 }
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -4,8 +4,9 @@ import { UserCardProps } from "../types/wikiListTypes";
 
 export default function UserCard({ article }: UserCardProps) {
   const userImage = article.image ? article.image : userImageNull;
+
   return (
-    <div className="w-[860px] h-[142px] bg-[#fff] my-[10px] m-auto rounded-25 shadow-lg py-[24px] ">
+    <article className="w-[860px] h-[142px] bg-[#fff] my-[10px] m-auto rounded-25 shadow-lg py-[24px] ">
       <div className="flex">
         <Image src={userImage} alt={article.image ? "유저 이미지" : "기본 유저 이미지"} className="flex-none mx-[32px] rounded-full" width={85} height={85} />
         <div className="flex-1 w-[150px] h-[100px]">
@@ -20,6 +21,6 @@ export default function UserCard({ article }: UserCardProps) {
         </div>
         <div className="flex-none mr-[32px]">link</div>
       </div>
-    </div>
+    </article>
   );
 }
