c86a413780d56b7f765248d1fcde6a49216f6df2
AdamSeungheonShin <ssh9809@naver.com>
2024-06-28 17:18:12 +0900

    feat: setup custom options in tailwind-config / change root in next-config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,9 @@ const config: Config = {
   content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      height: {
+        "400": "400px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
