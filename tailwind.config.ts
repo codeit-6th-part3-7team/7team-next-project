78c12a2c46c39219a66c17f15cdb446b38c377f7
Lee Youl <2yulrang@gmail.com>
2024-06-28 16:03:16 +0900

    fix: 테일윈드 config 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,8 +63,6 @@ const config: Config = {
       },
       borderRadius: {
         "1/2": "50%",
-        "20": "20px",
-        "25": "25px",
       },
       lineHeight: {
         "20": "1.25rem",
