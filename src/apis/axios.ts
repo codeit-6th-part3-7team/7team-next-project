95511d601800fc31a50ad1c32f82e2bccc856439
Lee Youl <2yulrang@gmail.com>
2024-06-28 18:25:32 +0900

    fix: axios 필요없는 부분 삭제

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTMxLCJ0ZWFtSWQiOiI2LTciLCJzY29wZSI6ImFjY2VzcyIsImlhdCI6MTcxOTU2NjI5NywiZXhwIjoxNzE5NTY4MDk3LCJpc3MiOiJzcC13aWtpZWQifQ.8K9QYCOiN0uihTjmWpB-r5FTDMV_jIIENGcMII4mXUA";
 const instance = axios.create({
-  baseURL: "https://wikied-api.vercel.app/6-7",
-  // baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
 export default instance;
