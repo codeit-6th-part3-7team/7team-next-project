ca868e06e1c750b1b31786506a74ddddc85a3b76
Lee Youl <2yulrang@gmail.com>
2024-06-29 17:42:05 +0900

    fix: 불필요한 부분 정리

--- pages/boards/[id]/index.tsx
+++ pages/boards/[id]/index.tsx
@@ -1,15 +1,13 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function ArticlePage({ article, writer }: { article: ArticleType; writer: WriterType }) {
+export default function ArticlePage() {
   const router = useRouter();
   const { id } = router.query;
 
   return (
     <>
       <div>
-        {article}
-        {writer}
         <Link href={`/boards/${id}/edit`}>수정하기</Link>
       </div>
       <div />
