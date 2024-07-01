843da83e8c0e70951fd1a246c8b6ad83f22cb90f
sohee <topa11uu@gmail.com>
2024-07-01 13:44:07 +0900

    rebase

--- pages/wiki/wikiList.tsx
+++ pages/wiki/wikiList.tsx
@@ -38,39 +38,41 @@ export default function WikiList() {
   const searchResults = value ? articles.filter((article) => article.name.toLowerCase().includes(value.toLowerCase())) : articles;
 
   return (
-    <div className="w-[350px] md:w-[700px] lg:w-[860px] m-auto">
+    <>
       <header>
         <div>헤더</div>
       </header>
-      <main>
-        <SearchFrom value={value} setValue={setValue} page={page} setPage={setPage} />
-        <section className="w-[340px] md:w-[700px] lg:w-[860px] mb-[60px] m-auto my-4 px-4 text-[16px] font-[400] text-gray-400">
-          {value && searchResults.length > 0 ? (
-            <p>
-              &quot;{value}&quot;님을 총<span className="text-green-200">&nbsp;{searchResults.length}</span>명 찾았습니다.
-            </p>
-          ) : (
-            <br />
-          )}
-        </section>
-        <section className="h-[470px] my-20">
-          {searchResults.length > 0 ? (
-            searchResults.map((article) => <UserCard key={article.id} articles={[article]} />)
-          ) : (
-            <div>
-              <div className="flex justify-center py-[32px] text-[18px] md:text-[20px] lg:text-[20px] font-[500] text-gray-400">
-                <div className="flex-none">&quot;</div>
-                <div className="flex-none max-w-[100px] md:max-w-[200px] lg:max-w-[350px] truncate">{value}</div>
-                <div className="flex-none">&quot;과&#47;와 일치하는 검색 결과가 없어요.</div>
+      <div className="w-[350px] md:w-[700px] lg:w-[860px] m-auto">
+        <main>
+          <SearchFrom value={value} setValue={setValue} page={page} setPage={setPage} />
+          <section className="w-[340px] md:w-[700px] lg:w-[860px] mb-[60px] m-auto my-4 px-4 text-[16px] font-[400] text-gray-400">
+            {value && searchResults.length > 0 ? (
+              <p>
+                &quot;{value}&quot;님을 총<span className="text-green-200">&nbsp;{searchResults.length}</span>명 찾았습니다.
+              </p>
+            ) : (
+              <br />
+            )}
+          </section>
+          <section className="h-[470px] my-20">
+            {searchResults.length > 0 ? (
+              searchResults.map((article) => <UserCard key={article.id} articles={[article]} />)
+            ) : (
+              <div>
+                <div className="flex justify-center py-[32px] text-[18px] md:text-[20px] lg:text-[20px] font-[500] text-gray-400">
+                  <div className="flex-none">&quot;</div>
+                  <div className="flex-none max-w-[100px] md:max-w-[200px] lg:max-w-[350px] truncate">{value}</div>
+                  <div className="flex-none">&quot;과&#47;와 일치하는 검색 결과가 없어요.</div>
+                </div>
+                <div className="w-[100px] md:w-[144px] lg:w-[144px] m-auto">
+                  <Image src={NoSearchImage} alt="검색 결과 없음 이미지" draggable="false" />
+                </div>
               </div>
-              <div className="w-[100px] md:w-[144px] lg:w-[144px] m-auto">
-                <Image src={NoSearchImage} alt="검색 결과 없음 이미지" draggable="false" />
-              </div>
-            </div>
-          )}
-        </section>
-        <footer>{searchResults.length > 0 ? <Pagination totalPages={totalPages} currentPage={page} onPageChange={(newPage) => setPage(newPage)} /> : <br />}</footer>
-      </main>
-    </div>
+            )}
+          </section>
+          <footer>{searchResults.length > 0 ? <Pagination totalPages={totalPages} currentPage={page} onPageChange={(newPage) => setPage(newPage)} /> : <br />}</footer>
+        </main>
+      </div>
+    </>
   );
 }
