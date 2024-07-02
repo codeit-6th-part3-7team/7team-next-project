96eb9c137a0c4e4c58ffe86c7d84db4f7cc2f10b
KimSeungRae <160004698+JustDevRae@users.noreply.github.com>
2024-07-02 10:15:34 +0900

    [Merge] epic/sign-up-page merge develop (#55)
    * feat: husky linter 설정 변경
    
    * feat: react-hook-form, zod 설치
    
    * feat: 회원가입 유효성 검사 기능 구현
    
    * chore: include next.config.mjs and .prettierrc.js in tsconfig.json
    
    * style: module.exports 세미콜론 추가
    
    * chore: tailwindCSS 속성 자동 정렬 플러그인 추가
    
    * chore: 커스텀 css 변수 및 다크모드 미디어 쿼리 제거
    
    * chore: axios 설치
    
    * feat: axios 인스턴스 생성
    
    * feat: 회원가입 기능 구현
    
    * chore: LF, CRLF 자동 포맷팅 규칙 추가
    
    * chore: tailwindCSS 커스텀 속성 추가 및 적용
    
    * feat: 회원가입 시 Notification 구현 및 mantine 적용
    
    * refactor: 회원가입 스키마와 타입 별도의 파일로 분류
    
    * style: 로그인 폼 제목 수정
    
    * feat: 회원가입시 토큰 할당 구현
    
    * feat: 토큰 검증 로직 구현
    
    * feat: 로그인시 refresh토큰 할당되도록 수정
    
    * design: 모바일 반응형 크기 수정
    
    * design: 유효성 검사에 따른 input bg 컬러 수정
    
    * feat:엑세스 토큰 유효시 페이지 리디렉션 구현
    로그인을 했을 경우 엑세스 토큰이 부여되고
    로컬 스토리지에 토큰이 존재하는 경우 로그인 & 회원가입 페이지 접근시
    랜딩 페이지로 리디렉션합니다.
    
    * fix: input focusout 시 <Text/> 컴포넌트 중첩 에러 수정
    
    * refactor: 회원가입 페이지 폼 수정
    
    * refactor: 로그인 페이지 수정 및 폴더 경로 설정
    
    * design: 로그인 & 회원가입 Notificaion 디자인 변경
    
    * style: 불필요 코드 제거
    
    * design: login input focus & placeholder 디자인 변경
    
    * design: signup  input focus & placeholder 디자인 변경
    
    * feat: 회원가입 페이지 Header 컴포넌트 추가
    
    * feat: 로그인 & 회원가입 시 Header 프로필 전환 구현
    
    * refactor: Header 컴포넌트 _app.tsx파일로 이동
    
    * fix: 로그인 및 회원가입 시 프로필 미전환 오류 수정
    
    * chore: develop conflict 사항 수정
    
    * refactor: Header 컴포넌트 랜더링 위치 변경
    
    * style: 회원가입 폼 공통 스타일 변수 mantine 컴포넌트에 병합
    
    * style: 로그인 폼 공통 스타일 변수 mantine 컴포넌트에 병합
    
    * refactor: 회원가입, 로그인 폼 유효성 검증 반응 변경
    
    * refactor: 회원가입 시 랜딩 페이지 리디렉션 시간 수정
    
    ---------
    
    Co-authored-by: leeyoul <2yulrang@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import { Notifications } from "@mantine/notifications";
 import "@mantine/notifications/styles.css";
 import type { AppProps } from "next/app";
 import theme from "@/src/styles/theme";
+import Header from "@/src/components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={theme}>
       <Notifications position="top-right" />
+      <Header />
       <Component {...pageProps} />
     </MantineProvider>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,39 @@
 import { Group, Menu } from "@mantine/core";
-
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import ImgLogo from "@/public/assets/img_logo.webp";
 import IcoBurger from "@/public/assets/ic_burger.svg";
+import IcoProfile from "@/public/assets/ic_profile.svg";
+import { notifications } from "@mantine/notifications";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathName = usePathname();
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    setLoggedIn(false);
+    notifications.show({
+      color: "green.2",
+      title: "로그아웃",
+      message: "",
+      autoClose: 2000,
+      withCloseButton: true,
+    });
+  };
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  }, [pathName]);
+
   return (
     <div className="h-20">
       <header className="fixed z-10 h-20 w-full bg-white px-10 drop-shadow-md md:px-20">
@@ -23,35 +51,88 @@ export default function Header() {
               </Link>
             </Group>
           </Group>
-          <Group>
-            <Group visibleFrom="sm">
-              <Link href="/login" className="text-sm text-gray-400">
-                로그인
-              </Link>
-            </Group>
-            <Group hiddenFrom="sm">
-              <Menu width={110} position="bottom" radius="md" shadow="md" withinPortal>
-                <Menu.Target>
-                  <Image src={IcoBurger} width={24} height={24} alt="메뉴" className="cursor-pointer" />
-                </Menu.Target>
 
-                <Menu.Dropdown>
+          {loggedIn ? (
+            <Menu width={120} position="bottom" radius="md" shadow="md" withinPortal>
+              <Group>
+                <Menu.Target>
+                  <Group>
+                    <Group hiddenFrom="sm">
+                      <Image src={IcoBurger} width={24} height={24} alt="메뉴" className="cursor-pointer" />
+                    </Group>
+                    <Group visibleFrom="sm">
+                      <Image src={IcoProfile} width={32} height={32} alt="프로필" className="cursor-pointer" />
+                    </Group>
+                  </Group>
+                </Menu.Target>
+              </Group>
+
+              <Menu.Dropdown>
+                <Menu.Item hiddenFrom="sm">
                   <Link href="/wikilist" className="flex justify-center p-2 text-14 text-gray-800">
                     위키 목록
                   </Link>
+                </Menu.Item>
+                <Menu.Item hiddenFrom="sm">
                   <Link href="/boards" className="flex justify-center p-2 text-14 text-gray-800">
                     자유게시판
                   </Link>
+                </Menu.Item>
+                <Menu.Item>
+                  <Link href="/notifications" className="flex justify-center p-2 text-14 text-gray-800">
+                    알림
+                  </Link>
+                </Menu.Item>
+                <Menu.Item>
                   <Link href="/mypage" className="flex justify-center p-2 text-14 text-gray-800">
+                    마이페이지
+                  </Link>
+                </Menu.Item>
+                <Menu.Item>
+                  <Link href="/account-settings" className="flex justify-center p-2 text-14 text-gray-800">
                     계정 설정
                   </Link>
-                  <Link href="/wiki" className="flex justify-center p-2 text-14 text-gray-800">
-                    내 위키
-                  </Link>
-                </Menu.Dropdown>
-              </Menu>
-            </Group>
-          </Group>
+                </Menu.Item>
+                <Menu.Item onClick={handleLogout}>
+                  <div className="flex justify-center p-2 text-14 text-gray-800">로그아웃</div>
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          ) : (
+            <>
+              <Group visibleFrom="sm">
+                <Link href="/login" className="text-sm text-gray-400">
+                  로그인
+                </Link>
+              </Group>
+              <Group hiddenFrom="sm">
+                <Menu width={120} position="bottom" radius="md" shadow="md" withinPortal>
+                  <Menu.Target>
+                    <Image src={IcoBurger} width={24} height={24} alt="메뉴" className="cursor-pointer" />
+                  </Menu.Target>
+                  <Menu.Dropdown>
+                    <Menu.Item>
+                      <Link href="/wikilist" className="flex justify-center p-2 text-14 text-gray-800">
+                        위키 목록
+                      </Link>
+                    </Menu.Item>
+
+                    <Menu.Item>
+                      <Link href="/boards" className="flex justify-center p-2 text-14 text-gray-800">
+                        자유게시판
+                      </Link>
+                    </Menu.Item>
+
+                    <Menu.Item>
+                      <Link href="/login" className="flex justify-center p-2 text-14 text-gray-800">
+                        로그인
+                      </Link>
+                    </Menu.Item>
+                  </Menu.Dropdown>
+                </Menu>
+              </Group>
+            </>
+          )}
         </Group>
       </header>
     </div>
