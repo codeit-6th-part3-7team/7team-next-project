47de726f726b953f1460a4a542bc963eed00255d
JustDevRae <ksrae165@gmail.com>
2024-07-03 14:39:48 +0900

    remove: 드롭다운 메뉴 중 계정 설정 버튼 삭제

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -88,11 +88,6 @@ export default function Header() {
                     마이페이지
                   </Link>
                 </Menu.Item>
-                <Menu.Item>
-                  <Link href="/account-settings" className="flex justify-center p-2 text-14 text-gray-800">
-                    계정 설정
-                  </Link>
-                </Menu.Item>
                 <Menu.Item onClick={handleLogout}>
                   <div className="flex justify-center p-2 text-14 text-gray-800">로그아웃</div>
                 </Menu.Item>
