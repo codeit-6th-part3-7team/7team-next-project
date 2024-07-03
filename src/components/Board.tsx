28ee7d93681943de6915e156b6f3e2c11ba72729
Lee Youl <2yulrang@gmail.com>
2024-07-03 14:59:05 +0900

    Design: 디자인 수정, 제목 길 경우를 대비하여 스타일 수정

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,6 @@
 import Parser from "html-react-parser";
 import Image from "next/image";
-import { ActionIcon, Button, Flex, Modal, useMatches } from "@mantine/core";
+import { ActionIcon, Box, Button, Divider, Flex, Modal, useMatches } from "@mantine/core";
 import { ChangeEvent, useState } from "react";
 import IcoPencil from "@/public/assets/ic_pencil.svg";
 import IcoBin from "@/public/assets/ic_bin.svg";
@@ -62,45 +62,51 @@ export default function Board({ initialValues, isMine }: BoardProps) {
   };
 
   return (
-    <Flex justify="center" align="center" w="100%">
-      <Flex direction="column" w={{ base: "100%", lg: 1060 }} mih="40vh" pt={46} pb={30} px={30} mx={{ base: 20, sm: 60, lg: 0 }} className="rounded-[10px] bg-white drop-shadow-md">
-        <h2 className="order-1 text-16 font-semibold text-gray-800 md:text-20 lg:text-24">{values?.title}</h2>
-        <Flex my={24} justify="space-between" className="order-3">
-          <p className="text-12 text-gray-400 md:text-16">
-            <strong className="mr-2 font-normal">{values?.writer.name}</strong>
-            <strong className="font-normal">{new Date(values?.updatedAt ?? "").toLocaleDateString()}</strong>
-          </p>
-          <label htmlFor="like" className="flex">
-            {like ? (
-              <Image src={IcoHeartOn} width={24} height={24} alt="아이콘" aria-hidden="true" className="mr-1" />
-            ) : (
-              <Image src={IcoHeart} width={24} height={24} alt="아이콘" aria-hidden="true" className="mr-1" />
+    <>
+      <Flex direction="column" justify="center" align="center" w={{ base: "100%", lg: 1060 }} px={{ base: 20, sm: 60, lg: 0 }}>
+        <Flex direction="column" w="100%" pt={46} pb={30} px={30} className="rounded-[10px] bg-white drop-shadow-md">
+          <Box mb={{ base: 14, sm: 30 }}>
+            <Box className="float-left overflow-hidden">
+              <h2 className="text-24 font-semibold text-gray-800 md:text-32">{values?.title}</h2>
+            </Box>
+            {isMine && (
+              <Flex gap={{ base: 12, lg: 14 }} className="float-right">
+                <Button href={`/boards/${values?.id}/edit`} component={Link} type="submit" color={btnColor} size={btnSize} className="mantine-visible-from-sm">
+                  수정하기
+                </Button>
+                <ActionIcon variant="transparent" aria-label="수정하기" className="mantine-hidden-from-sm">
+                  <Image src={IcoPencil} width={24} height={24} alt="아이콘" aria-hidden="true" />
+                </ActionIcon>
+                <Button type="submit" color={btnColor} size={btnSize} className="mantine-visible-from-sm" onClick={openDeleteModal}>
+                  삭제하기
+                </Button>
+                <ActionIcon variant="transparent" aria-label="삭제하기" className="mantine-hidden-from-sm" onClick={openDeleteModal}>
+                  <Image src={IcoBin} width={24} height={24} alt="아이콘" aria-hidden="true" />
+                </ActionIcon>
+              </Flex>
             )}
-            <span className="text-gray-400">{likeCount}</span>
-          </label>
-          <input type="checkbox" id="like" checked={like} onChange={handleHeart} className="hidden" />
-        </Flex>
-        <Flex direction="column" py={{ base: 16, sm: 20 }} className="order-4">
-          {Parser(values?.content ?? "")}
-        </Flex>
-
-        <Flex gap={{ base: 12, lg: 14 }} mt={-31} className="order-2 self-end">
-          {isMine && (
-            <>
-              <Button href={`/boards/${values?.id}/edit`} component={Link} type="submit" color={btnColor} size={btnSize} className="mantine-visible-from-sm">
-                수정하기
-              </Button>
-              <ActionIcon variant="transparent" aria-label="수정하기" className="mantine-hidden-from-sm">
-                <Image src={IcoPencil} width={24} height={24} alt="아이콘" aria-hidden="true" />
-              </ActionIcon>
-              <Button type="submit" color={btnColor} size={btnSize} className="mantine-visible-from-sm" onClick={openDeleteModal}>
-                삭제하기
-              </Button>
-              <ActionIcon variant="transparent" aria-label="삭제하기" className="mantine-hidden-from-sm" onClick={openDeleteModal}>
-                <Image src={IcoBin} width={24} height={24} alt="아이콘" aria-hidden="true" />
-              </ActionIcon>
-            </>
-          )}
+          </Box>
+          <Flex justify="space-between">
+            <p className="text-12 text-gray-400 md:text-14">
+              <strong className="mr-2 font-normal">{values?.writer.name}</strong>
+              <strong className="font-normal">{new Date(values?.updatedAt ?? "").toLocaleDateString()}</strong>
+            </p>
+            <label htmlFor="like" className="flex items-center gap-[2px]">
+              <Flex w={{ base: 16, sm: 18 }} h={{ base: 16, sm: 18 }}>
+                {like ? (
+                  <Image src={IcoHeartOn} style={{ width: "100%" }} alt="아이콘" aria-hidden="true" className="mr-1" />
+                ) : (
+                  <Image src={IcoHeart} style={{ width: "100%" }} alt="아이콘" aria-hidden="true" className="mr-1" />
+                )}
+              </Flex>
+              <span className="text-12 text-gray-400 md:text-14">{likeCount}</span>
+            </label>
+            <input type="checkbox" id="like" checked={like} onChange={handleHeart} className="hidden" />
+          </Flex>
+          <Divider color="#E4E5F0" mt={10} />
+          <Flex direction="column" py={{ base: 16, sm: 20 }} className="text-gray-800">
+            {Parser(values?.content ?? "")}
+          </Flex>
         </Flex>
       </Flex>
       <Modal opened={deleteModalOpened} onClose={closeDeleteModal} size="xs" centered>
@@ -116,6 +122,6 @@ export default function Board({ initialValues, isMine }: BoardProps) {
           </Flex>
         </Flex>
       </Modal>
-    </Flex>
+    </>
   );
 }
