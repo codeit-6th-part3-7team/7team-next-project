1f09a56b318601829868f4391023c9ade600e79d
JustDevRae <ksrae165@gmail.com>
2024-07-03 14:50:46 +0900

    feat: 위키 생성하기 폼 구현

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,5 +1,11 @@
 import ChangePasswordForm from "@/src/components/ChangePasswordForm";
+import CreateWikiForm from "@/src/components/CreateWikiForm";
 
 export default function MyPage() {
-  return <ChangePasswordForm />;
+  return (
+    <>
+      <ChangePasswordForm />
+      <CreateWikiForm />
+    </>
+  );
 }
