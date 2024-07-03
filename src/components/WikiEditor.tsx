1e687f35aa508fbd4287378147fb7863492dea64
Lee Youl <2yulrang@gmail.com>
2024-07-03 17:03:37 +0900

    fix: 충돌 해결

--- src/components/WikiEditor.tsx
+++ src/components/WikiEditor.tsx
@@ -54,7 +54,7 @@ export default function WikiEditor({ initialData, title, handleChangeContent }: 
       {editor && <WikiEditorMenu editor={editor} title={title} />}
       {/* note prettier 설정에서 강제 정렬 수정으로 오류 발생해서 해당 부분 제외 했습니다 */}
       {/* eslint-disable-next-line */}
-      <div className="prose prose-sm md:prose-base min-h-[600px] max-w-none overflow-auto rounded-[10px] px-5 py-10 shadow-lg shadow-gray-200">
+      <div className="prose prose-sm min-h-[600px] max-w-none overflow-auto rounded-[10px] px-5 py-10 shadow-lg shadow-gray-200 md:prose-base">
         <EditorContent
           editor={editor}
           style={{
