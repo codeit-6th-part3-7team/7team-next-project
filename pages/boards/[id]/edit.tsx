98740e8bff48f8eb45774af51fc7e453dfd20c0a
Lee Youl <2yulrang@gmail.com>
2024-07-03 16:51:04 +0900

    Refactor: 에러, 로딩처리 추가, 예외처리

--- pages/boards/[id]/edit.tsx
+++ pages/boards/[id]/edit.tsx
@@ -1,32 +1,9 @@
 import WriteBoard from "@/src/components/WriteBoard";
-import { Button, Flex } from "@mantine/core";
-import axios from "@/src/apis/axios";
+import { Button, Flex, Loader } from "@mantine/core";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
-import { GetServerSidePropsContext } from "next";
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const { id } = context.query;
-  let article;
-  let writer;
-  try {
-    const articleRes = await axios.get(`/articles/${id}`);
-    article = articleRes.data ?? [];
-    writer = articleRes.data.writer ?? [];
-  } catch {
-    return {
-      notFound: true,
-    };
-  }
-  return {
-    props: {
-      id,
-      article,
-      writer,
-    },
-  };
-}
+import { useCallback, useEffect, useState } from "react";
+import instance from "@/src/apis/axios";
 
 interface ArticleType {
   title: string;
@@ -39,24 +16,83 @@ interface WriterType {
   name: string;
 }
 
-export default function EditBoard({ id, article, writer }: { id: number; article: ArticleType; writer: WriterType }) {
-  const [value] = useState({
-    title: article.title,
-    content: article.content,
-    image: article.image,
-    writer: writer.name,
-    updatedAt: article.updatedAt,
+export default function EditBoard() {
+  const [values, setValues] = useState({
+    title: "",
+    content: "",
+    image: "",
+    writer: "",
+    updatedAt: new Date(),
   });
-  const router = useRouter();
 
-  const handleSubmit = async (values: { title: string; content: string; image?: string }) => {
-    await axios.patch(`/articles/${id}`, values);
-    router.push(`/boards/${id}`);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<any>(null);
+  const router = useRouter();
+  const { id } = router.query;
+
+  const handleLoad = useCallback(async () => {
+    if (!id) {
+      return;
+    }
+    setLoading(true);
+    try {
+      const userRes = await instance.get(`/users/me`);
+      const articleRes = await instance.get(`/articles/${id}`);
+      const { title, content, image, writer, updatedAt } = articleRes.data;
+      if (userRes.data.id === articleRes.data.writer.id) {
+        setValues({ title, content, image, writer: writer.name, updatedAt });
+      } else {
+        router.push(`/boards/${id}`);
+      }
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  }, [id]);
+
+  const handleSubmit = async (data: { title: string; content: string; image?: string }) => {
+    if (!id) {
+      return;
+    }
+
+    setLoading(true);
+    try {
+      await instance.patch(`/articles/${id}`, data);
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+      router.push(`/boards/${id}`);
+    }
   };
+
+  useEffect(() => {
+    handleLoad();
+  }, [handleLoad]);
+
+  if (loading) {
+    return (
+      <Flex justify="center" align="center" mih={{ base: "calc(100vh - 60px)", sm: "calc(100vh - 80px)" }}>
+        <Loader size="md" />
+      </Flex>
+    );
+  }
+
+  if (error) {
+    return (
+      <Flex direction="column" justify="center" align="center" mih={{ base: "calc(100vh - 60px)", sm: "calc(100vh - 80px)" }}>
+        <p className="text-red-200">{error?.message}</p>
+        <Button href="/boards" component={Link} variant="outline" color="green" mt={4}>
+          목록으로
+        </Button>
+      </Flex>
+    );
+  }
 
   return (
     <Flex direction="column">
-      <WriteBoard type="edit" initialValues={value} onSubmit={handleSubmit} />
+      <WriteBoard type="edit" initialValues={values} onSubmit={handleSubmit} />
       <Flex justify="center" h={50}>
         <Button href="/boards" component={Link} variant="outline" color="green" px={40}>
           목록으로
