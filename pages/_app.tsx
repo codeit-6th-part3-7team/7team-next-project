a221b292d53eda32693e86b9b5145f8620af1e33
Lee Youl <2yulrang@gmail.com>
2024-07-05 16:31:05 +0900

    fix: 중복 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import "@mantine/notifications/styles.css";
 import type { AppProps } from "next/app";
-import theme from "@/src/styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
