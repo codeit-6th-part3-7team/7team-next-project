8a016b6c27f8107b8d0b031aa766041f35173f3e
sohee <topa11uu@gmail.com>
2024-07-05 15:35:19 +0900

    Docs: 불필요한 코드 삭제

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -83,7 +83,6 @@ const config: Config = {
       },
       boxShadow: {
         lg: "0 4px 20px 0 rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
-        inner: "inset 0 2px 4px 0 rgb(0 0 0 / 0.05);",
       },
     },
   },
